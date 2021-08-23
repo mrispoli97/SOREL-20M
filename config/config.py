b35881f65b92b768cd520b0d8267731981bbcedf
Mario <mariorispoli97@hotmail.it>
2021-08-23 12:54:07 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.dirname(os.path.realpath(__file__).parent.absolute())
 BINARY_DATABASE = r'C:\Users\mario\Downloads\meta.db'
 FEATURES_DATABASE = r'C:\Users\mario\Downloads\data.mdb'
